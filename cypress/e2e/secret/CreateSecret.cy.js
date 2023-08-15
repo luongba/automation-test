c459e427f7551503d969004fc7e1c70da7c5cf70
vinhdx <vinhdao992000@gmail.com>
2023-08-15 10:52:26 +0700

    feat!: add test case for secret tab

--- cypress/e2e/secret/CreateSecret.cy.js
+++ cypress/e2e/secret/CreateSecret.cy.js
@@ -1,5 +1,36 @@
 describe('Chức năng 1: Tạo Secret', () => {
-    it('Kiểm tra Tạo thành công', () => {
-      cy.createSecretSuccess("secret/TL_33_data_test.json")
-    })
+  beforeEach(() => {
+    Cypress.env("environment") === "production"
+    ? cy.loginProduction()
+    : cy.loginDevelopment();
+    cy.visit("/key-manager/secrets");
+    cy.get('.ant-card-head button').click(); 
+  });
+  it('Kiểm tra Tạo thành công', () => {
+    cy.createSecretSuccess("secret/TL_33_data_test.json")
+  })
+  it('Kiểm tra Tạo không thành công (để trông thông tin)', () => {
+    cy.createSecretFaild34("secret/TL_34_data_test.json")
+  })
+  it('Kiểm tra Tạo không thành công (trùng key)', () => {
+    cy.createSecretFaild35("secret/TL_35_data_test.json")
+  })
+  it('Kiểm tra Tạo không thành công (Ấn hủy)', () => {
+    cy.createSecretFaild36("secret/TL_36_data_test.json")
+  })
+})
+
+describe('Chức năng 2: Danh sách Secrets', () => {
+  beforeEach(() => {
+    Cypress.env("environment") === "production"
+    ? cy.loginProduction()
+    : cy.loginDevelopment();
+    cy.visit("/key-manager/secrets");
+  });
+  it('Kiểm tra thực hiện tìm kiếm (từ khóa tồn tại)', () => {
+    cy.findValidSecret("secret/TL_38_data_test.json")
+  })
+  it('Kiểm tra thực hiện tìm kiếm (từ khóa không tồn tại)', () => {
+    cy.findInvalidSecret("secret/TL_39_data_test.json")
+  })
 })
--- cypress/support/secret.js
+++ cypress/support/secret.js
@@ -1,17 +1,90 @@
 // TL_33
 Cypress.Commands.add("createSecretSuccess", (fileData) => {
-    Cypress.env("environment") === "production"
-      ? cy.loginProduction()
-      : cy.loginDevelopment();
-    cy.visit("/key-manager/secrets");
-    cy.get('.ant-card-head button').click(); 
-    cy.fixture(fileData).then((data) => {
-        cy.get('#AddSecretsForm_name').type(
-            data.secret_name
-        );
-        for (let i = 0; i < data.secret_value.length; i++) {
-          cy.get(`#formSecretValue_[${i}].key`).type(data.secret_value[i].key)
-          cy.get(`#formSecretValue_[${i}].value`).type(data.secret_value[i].value)
-        }
-    })
+  cy.fixture(fileData).then((data) => {
+    cy.get("#AddSecretsForm_name").type(data.secret_name);
+    cy.get(`tr[data-row-key="0"]`)
+      .find("input.ant-input")
+      .eq(0)
+      .type(data.secret_value[0].key);
+    cy.get(`tr[data-row-key="0"]`)
+      .find("input.ant-input")
+      .eq(1)
+      .type(data.secret_value[0].value);
   });
+  cy.get("#buttonCreate").click();
+  cy.get(".ant-notification-notice-message")
+    .contains("Tạo Secret thành công")
+    .should("be.visible");
+});
+// TL_34
+Cypress.Commands.add("createSecretFaild34", (fileData) => {
+  cy.fixture(fileData).then((data) => {
+    cy.get("#AddSecretsForm_name").type(data.secret_name);
+  });
+  cy.get("#buttonCreate").click();
+  cy.get(`tr[data-row-key="0"]`)
+    .find(".ant-form-explain")
+    .contains("Vui lòng nhập thông tin");
+});
+// TL_35
+Cypress.Commands.add("createSecretFaild35", (fileData) => {
+  cy.fixture(fileData).then((data) => {
+    cy.get("#AddSecretsForm_name").type(data.secret_name);
+    for (let i = 0; i < data.secret_value.length; i++) {
+      try {
+        if (i !== 0) {
+          cy.get(".ant-tabs-content button").click();
+        }
+        cy.get(`tr[data-row-key="${i}"]`)
+          .find("input.ant-input")
+          .eq(0)
+          .type(data.secret_value[i].key);
+        cy.get(`tr[data-row-key="${i}"]`)
+          .find("input.ant-input")
+          .eq(1)
+          .type(data.secret_value[i].value);
+      } catch (error) {}
+    }
+    cy.get(`tr[data-row-key="${data.secret_value.length - 1}"]`)
+      .find(".ant-form-explain")
+      .contains("Key bị trùng lặp! Vui lòng thay đổi Key ");
+  });
+});
+// TL_36
+Cypress.Commands.add("createSecretFaild36", (fileData) => {
+  cy.fixture(fileData).then((data) => {
+    cy.get("#AddSecretsForm_name").type(data.secret_name);
+    cy.get(`tr[data-row-key="0"]`)
+      .find("input.ant-input")
+      .eq(0)
+      .type(data.secret_value[0].key);
+    cy.get(`tr[data-row-key="0"]`)
+      .find("input.ant-input")
+      .eq(1)
+      .type(data.secret_value[0].value);
+  });
+  cy.get(".page--footer .btn-button-tertiary").click();
+  cy.url()
+    .should('match', /\/key-manager\/secrets/)
+});
+// TL_38
+Cypress.Commands.add("findValidSecret", (fileData) => {
+  cy.fixture(fileData).then((data) => {
+    cy.get('input[placeholder="Tìm kiếm theo tên Secret"]').type(
+      data.secret_search
+    );
+    cy.get('input[placeholder="Tìm kiếm theo tên Secret"]').type("{enter}");
+    cy.get('.ant-table-row').should('have.length.gt', 0)
+  });
+});
+
+// TL_39
+Cypress.Commands.add("findInvalidSecret", (fileData) => {
+  cy.fixture(fileData).then((data) => {
+    cy.get('input[placeholder="Tìm kiếm theo tên Secret"]').type(
+      data.secret_search
+    );
+    cy.get('input[placeholder="Tìm kiếm theo tên Secret"]').type("{enter}");
+    cy.get('.ant-table-placeholder').contains("Không tìm thấy dữ liệu").should("be.visible");
+  });
+});
